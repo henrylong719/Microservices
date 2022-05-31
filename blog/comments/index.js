c7bded9cfd6e75867553c70052558009aee5434e
henrylong <henrylong719@gmail.com>
2022-05-31 21:53:45 +0930

    add backend post and comments

---
+++ blog/comments/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+
+const commentsByPostId = {};
+app.use(bodyParser.json());
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({
+    id: commentId,
+    content,
+  });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Example app listening on port 4001!');
+});

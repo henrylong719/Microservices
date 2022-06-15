7096bf789b7be741fc8da48c8112a094fbd4773f
henrylong <henrylong719@gmail.com>
2022-06-15 22:52:38 +0930

    add deploy

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -14,7 +14,7 @@ app.post('/events', async (req, res) => {
 
       console.log(status);
 
-      await axios.post('http://localhost:4005/events', {
+      await axios.post('http://event-bus-srv:4005/events', {
         type: 'CommentModerated',
         data: {
           id: data.id,

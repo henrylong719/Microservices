be56fb94addb698199d534c82e7cc91fd1fccd52
henrylong <henrylong719@gmail.com>
2022-07-04 23:06:14 +0930

    add user model

---
+++ tickting/auth/src/models/user.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+// An interface that describe the properties that are required to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 20,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+const buildUser = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+export { User, buildUser };

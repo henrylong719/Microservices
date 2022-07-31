fb902e46a42a790a5b9155fa9ea7fe7f939314ab
henrylong719 <henrylong719@gmail.com>
2022-07-31 17:35:46 +0930

    add header

--- tickting/client/pages/index.js
+++ tickting/client/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { buildClient } from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
   return currentUser ? (
     <h1>You are signed in</h1>
   ) : (
@@ -11,6 +10,7 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async (context) => {
+  console.log('this is index page');
   const client = buildClient(context);
 
   const { data } = await client.get('/api/users/currentuser');

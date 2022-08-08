9c7614ff5fd6eeafdaf4d2d20ebcb29a9f2e52a3
henrylong719 <henrylong719@gmail.com>
2022-08-08 23:02:10 +0930

    add tests

--- tickting/tickets/src/app.ts
+++ tickting/tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@hltickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@hltickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +15,10 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();

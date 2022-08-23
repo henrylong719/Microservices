04254316ee9f25b37f88acf5aafd3c21c2cf6917
henrylong719 <henrylong719@gmail.com>
2022-08-23 22:17:58 +0930

    add types for onMessage

--- tickting/nats-test/src/events/ticket-created-listener.ts
+++ tickting/nats-test/src/events/ticket-created-listener.ts
@@ -4,10 +4,14 @@ import { Subjects } from './subjects';
 import { TicketCreatedEvent } from './ticket-created-event';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data!', data);
+
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
     msg.ack();
   }
 }

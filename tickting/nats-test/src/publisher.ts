a2cb858d753ea3a421300d8bc1d3b19de683911c
henrylong719 <henrylong719@gmail.com>
2022-08-23 22:55:37 +0930

    add promise for publish

--- tickting/nats-test/src/publisher.ts
+++ tickting/nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -6,17 +7,28 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
+  try {
+    const publisher = new TicketCreatedPublisher(stan);
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-    // stan.close();
-  });
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  //   // stan.close();
+  // });
 });

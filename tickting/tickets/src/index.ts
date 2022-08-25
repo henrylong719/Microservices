0d320b578093226c5de6ffcee5752a36d4f928ce
henrylong719 <henrylong719@gmail.com>
2022-08-25 22:39:57 +0930

    fix nats typo

--- tickting/tickets/src/index.ts
+++ tickting/tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -11,6 +12,14 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', 'alsdkj', 'http://nats-srv:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     console.log('connect to mongodb');
     await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
--- tickting/tickets/src/routes/new.ts
+++ tickting/tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { requireAuth, validateRequest } from '@hltickets/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publsiher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -25,6 +27,13 @@ router.post(
     });
 
     await ticket.save();
+
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }

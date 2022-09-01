453e5200e79d0fad4a44941b115ece0402a77e1d
henrylong719 <henrylong719@gmail.com>
2022-09-01 22:59:34 +0930

    get order service working

--- tickting/tickets/src/index.ts
+++ tickting/tickets/src/index.ts
@@ -11,8 +11,24 @@ const start = async () => {
     throw new Error('MONGO_URI is not defined');
   }
 
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID is not defined');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL is not defined');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID is not defined');
+  }
+
   try {
-    await natsWrapper.connect('ticketing', 'alsdkj', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();

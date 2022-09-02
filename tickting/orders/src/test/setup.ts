79d5a102c0d4638f2121f5ae8e5df56e06b111e1
henrylong719 <henrylong719@gmail.com>
2022-09-02 16:55:20 +0930

    add new route tests

---
+++ tickting/orders/src/test/setup.ts
@@ -0,0 +1,49 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+jest.mock('../nats-wrapper');
+
+let mongo: any;
+declare global {
+  var signin: () => string[];
+}
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  if (mongo) {
+    await mongo.stop();
+  }
+});
+
+global.signin = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = { jwt: token };
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  return [`session=${base64}`];
+};

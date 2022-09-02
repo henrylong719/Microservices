c54df5756a075187bb979c50ccf1a405db8fcbab
henrylong719 <henrylong719@gmail.com>
2022-09-02 16:31:48 +0930

    add new order route handler

--- tickting/orders/src/routes/new.ts
+++ tickting/orders/src/routes/new.ts
@@ -12,6 +12,7 @@ import { Order } from '../models/order';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   '/api/orders',
@@ -39,7 +40,19 @@ router.post(
       throw new BadRequestError('Ticket is already reserved');
     }
 
-    res.send({});
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+
+    res.status(201).send(order);
   }
 );
 

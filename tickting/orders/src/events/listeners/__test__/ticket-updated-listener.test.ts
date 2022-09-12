9e3f09028b43064f7d0c04a6e376920212194668
henrylong719 <henrylong719@gmail.com>
2022-09-12 09:38:56 +0930

    add ticket create and update listeners

---
+++ tickting/orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,35 @@
+import { TicketUpdatedEvent } from '@hltickets/common';
+import mongoose from 'mongoose';
+import { Ticket } from '../../../models/ticket';
+import { natsWrapper } from '../../../nats-wrapper';
+import { TicketUpdatedListener } from '../ticket-updated-listener';
+
+const setup = async () => {
+  // Create an instance of the listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  // Create a fake data event
+  const data: TicketUpdatedEvent['data'] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: 'new concert',
+    price: 999,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+
+  // Create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg, ticket };
+};

33a5af3bbfde4e7b0938075331bf22e882990137
henrylong719 <henrylong719@gmail.com>
2022-10-06 16:58:27 +1030

    add order cancelled listener and tests

---
+++ tickting/payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,53 @@
+import { OrderCancelledEvent, OrderStatus } from '@hltickets/common';
+import mongoose from 'mongoose';
+import { Order } from '../../../models/order';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCancelledListener } from '../order-cancelled-listener';
+
+const setup = async () => {
+  // Create a fake data event
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    price: 10,
+    userId: 'asdasd',
+    version: 0,
+  });
+
+  await order.save();
+
+  const data: OrderCancelledEvent['data'] = {
+    id: order.id,
+    version: 1,
+    ticket: {
+      id: 'asdasd',
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg, order };
+};
+
+it('updates the status of the order', async () => {
+  const { listener, data, msg, order } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('acks the message', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
--- tickting/payments/src/index.ts
+++ tickting/payments/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
@@ -35,6 +36,9 @@ const start = async () => {
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCreatedListener(natsWrapper.client).listen();
 
     console.log('connect to mongodb');
     await mongoose.connect(process.env.MONGO_URI);
